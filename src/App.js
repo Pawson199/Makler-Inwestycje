a3a18a9c80af841dc59a8e6045f0105276cd8197
Pawson199 <starnaw1998@interia.pl>
2020-05-03 16:16:22 +0200

    setted react-router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
-
+import { Switch, Link, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Oferta from './pages/Oferta'
+import Kontakt from './pages/Kontakt'
 
 function App() {
   return (
-    <div>
+    <navbar>
+   <ul>
+    <Link to="/home" ><li>HOME</li></Link>
+    <Link to="/oferta" ><li>OFERTY</li></Link>
+    <Link to="/about" ><li>O NAS</li></Link>
+    <Link to="/kontakt" ><li>KONTAKT</li></Link>
+   </ul>
 
-        <p>
-      STRONA MAKLERA
-        </p>
+    <Switch>
+      <Route path="/home"> <Home/> </Route>
+      <Route path="/oferta"> <Oferta/> </Route>
+      <Route path="/about"> <About/> </Route>
+      <Route path="/kontakt"> <Kontakt/> </Route>
+    </Switch>
+    </navbar>
+  )
 
-    </div>
-  );
 }
 
 export default App;
