a0f77f89357a2edc73a46a9cc72103a5ba95fba9
Pawson199 <starnaw1998@interia.pl>
2020-05-04 13:48:55 +0200

    menu pushed to its own component

--- src/App.js
+++ src/App.js
@@ -1,49 +1,20 @@
-import React, {useState} from 'react';
-import { Switch, Link, Route} from 'react-router-dom'
+import React from 'react';
+import { Switch, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Oferta from './pages/Oferta'
 import Kontakt from './pages/Kontakt'
 import logo from './images/logo-makler.png'
+import Menu from './components/Menu'
 
 function App() {
-
-  const [permission, setPermission] = useState("display_off")
-
-  const setPermissionOnClick = () => {
-    setPermission( 
-      prevState => prevState === "display_off" ?  "display_in" : 'display_off'
-    )
-  }
-  
-  const burger_menu = () => { return ( 
-      <>
-        <button onClick={setPermissionOnClick} className="burger" > MENU </button>
-        <ul onClick={setPermissionOnClick} className={`${permission} burgerek`}>
-          <li><Link to="/home" >HOME</Link></li>
-          <li><Link to="/oferta" >OFERTY</Link></li>
-          <li><Link to="/about" >O NAS</Link></li>
-          <li><Link to="/kontakt" >KONTAKT</Link></li>
-        </ul>
-      </>
-    )
-  }
-
-  const classic_menu = () => { return ( 
-    <ul>
-      <li><Link to="/home" >HOME</Link></li>
-      <li><Link to="/oferta" >OFERTY</Link></li>
-      <li><Link to="/about" >O NAS</Link></li>
-      <li><Link to="/kontakt" >KONTAKT</Link></li>
-    </ul>)
-  }
 
   return (
     <>
 
    <div className='menu' >
     <img alt="logo" src={logo}/>
-    {  window.innerWidth > 700 ? classic_menu() : burger_menu() }
+    <Menu/>
    </div>
 
    <div className='container'>
---
+++ src/components/Menu.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom'
+
+export default function Menu(){
+
+
+    const [permission, setPermission] = useState("display_off")
+
+    const setPermissionOnClick = () => {
+      setPermission( 
+        prevState => prevState === "display_off" ?  "display_in" : 'display_off'
+      )
+    }
+     const burger_menu = () => { return ( 
+        <>
+          <button onClick={setPermissionOnClick} className="burger" > MENU </button>
+          <ul onClick={setPermissionOnClick} className={`${permission} burgerek`}>
+            <li><Link to="/home" >HOME</Link></li>
+            <li><Link to="/oferta" >OFERTY</Link></li>
+            <li><Link to="/about" >O NAS</Link></li>
+            <li><Link to="/kontakt" >KONTAKT</Link></li>
+          </ul>
+        </>
+      )
+    }
+    
+     const classic_menu = () => { return ( 
+      <ul>
+        <li><Link to="/home" >HOME</Link></li>
+        <li><Link to="/oferta" >OFERTY</Link></li>
+        <li><Link to="/about" >O NAS</Link></li>
+        <li><Link to="/kontakt" >KONTAKT</Link></li>
+      </ul>)
+    }
+    
+    return( 
+       window.innerWidth > 700 ? classic_menu() : burger_menu()
+        )
+    
+}
+
