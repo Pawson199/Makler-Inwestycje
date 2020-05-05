4c7bdd425df09f9691d560ff86feb2979f662c35
Pawson199 <starnaw1998@interia.pl>
2020-05-05 20:51:04 +0200

    1 part of homepage - css - image and description

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,17 +4,18 @@ import homomom from '../images/home.svg'
 export default function Home() {
     return (
         <>
-            <div className="home_intro" >
                 <span className="home_desc">
-                <h1>Zaoszczędź czas i pieniądze</h1>
-                    <h2>Zaprojektuj z nami wymarzony dom, <br/> mieszkanie, lub odbierz gotowy projekt!</h2>
-                    <button className="home_intro_button" >NAPISZ DO NAS</button>
-                    <h3>OFERTY</h3>
-                </span>
-                <div className="home_image" >
+                    <div className="desc_holder" >
+                        <h1>Zaoszczędź czas i pieniądze</h1>
+                        <h2>Zaprojektuj z nami wymarzony dom, mieszkanie, lub odbierz gotowy projekt!</h2>
+                        <span className="button_holder">
+                            <button className="home_intro_button" >NAPISZ DO NAS</button>  
+                        </span>   
+                    </div>
+                    <div className="home_image">
                     <img src={homomom} ></img>
-                </div>
-            </div>
+                    </div>
+                </span>  
             <div>oferty</div>
         </>
     )
