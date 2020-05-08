301a6e3258b92246d9cd529778e649176d8bb0f9
Pawson199 <starnaw1998@interia.pl>
2020-05-08 21:33:57 +0200

    responsive swiper 2/3 part of homepage

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import GliderComponent from 'react-glider-carousel';
 
-export default function Component() {
+export default function Component(props) {
+
   return (
-    <GliderComponent hasArrows={true} settings={ {slidesToShow : 2, slidesToScroll : 1}}  >
-      <div className="dic">Slide 1</div>
-      <div className="dic">Slide 2</div>
-      <div className="dic">Slide 3</div>
-      <div className="dic">Slide 4</div>
+    <GliderComponent hasArrows={true} settings={ {slidesToShow : props.number, slidesToScroll : 1}}  >
+      <div><div className="dic">Slide 2</div></div>
+      <div><div className="dic">Slide 3</div></div>
+      <div><div className="dic">Slide 4</div></div>
+      <div><div className="dic">Slide 5</div></div>
+      <div><div className="dic">Slide 7</div></div>
+      <div><div className="dic">Slide 9</div></div>
     </GliderComponent>
   )
 }
