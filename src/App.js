85f454368b7d742bcec7ef2252b93320d8de32e3
Pawson199 <starnaw1998@interia.pl>
2020-05-09 20:11:36 +0200

    improved mobile view

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import { Switch, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
@@ -10,6 +10,24 @@ import Footer from './components/Footer'
 
 function App() {
 
+  function first_amount_of_slides(){
+    if(window.innerWidth < 600 ){ return 1 }
+    else if(window.innerWidth > 600 & window.innerWidth < 1000 ){ return 2 }
+    else if(window.innerWidth > 1200){return 3}
+}
+const [slides, setslides] = useState(first_amount_of_slides())
+const change_quentity_of_slides = () => {
+    if(window.innerWidth < 600 ){ setslides(1) }
+    else if(window.innerWidth > 600 & window.innerWidth < 1000 ){ setslides(2) }
+    else if(window.innerWidth > 1200){setslides(3)}
+   }
+ useEffect(() => {
+   window.addEventListener('resize', change_quentity_of_slides )
+   return () => {
+    window.removeEventListener('resize', change_quentity_of_slides )
+   }
+}, [])
+
   return (
     <>
 
@@ -20,7 +38,7 @@ function App() {
 
    <div className='container'>
     <Switch>
-        <Route exact path="/"> <Home/> </Route>
+        <Route exact path="/"> <Home slides={slides}/> </Route>
         <Route path="/oferta"> <Oferta/> </Route>
         <Route path="/about"> <About/> </Route>
         <Route path="/kontakt"> <Kontakt/> </Route>
