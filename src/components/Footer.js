b750b21eb7d1eb10a91e8359ef7830c4ad77bf88
Pawson199 <starnaw1998@interia.pl>
2020-05-11 20:12:12 +0200

    styled footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div>
-            Jestę Futerę :)
-        </div>
+        <h2 className="footer" >
+            © 2020 Makler Nieruchomości. |  Łódź 99-225 - lok. 404 - Pomorska 83/85 
+        </h2>
     )
 }
