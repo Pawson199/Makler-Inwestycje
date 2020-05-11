a18decad6383c56c0457cfc474a212cb052f968c
Pawson199 <starnaw1998@interia.pl>
2020-05-11 20:55:10 +0200

    last version of homepage

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ export default function Card(props) {
     return (
         <div className="dic">
             <span className="dic_image_container">
-                <img className="dicimage" src={props.src}></img>
+                <img className="dicimage" alt={props.name} src={props.src}></img>
             </span>
             <h1>{props.description}</h1>
             <span className="dic_button_container" >
