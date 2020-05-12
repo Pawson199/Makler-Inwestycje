f4f525674b650f4f3878acf25c6aee57f2729a8f
Pawson199 <starnaw1998@interia.pl>
2020-05-12 14:46:10 +0200

    staff cards in contact

--- src/pages/Kontakt.js
+++ src/pages/Kontakt.js
@@ -2,8 +2,28 @@ import React from 'react'
 
 export default function Kontakt() {
     return (
-        <div>
-            sdaffsda
-        </div>
+        <>
+            <label className="label">
+                <h1 className="label1" >Kontakt</h1>
+                <h1 className="label2" >Kontakt</h1>
+                </label>
+            <span className="employees">
+                <div>
+                    <h1>Dział Techniczny</h1>
+                    <p>Numer Kontaktowy</p>
+                    <p>323323323</p>
+                    <p>Mail</p>
+                    <p>Kamila@dsdas.pl</p>
+                </div>
+                <div>
+                    <h1>Agent Nieruchomości</h1>
+                    <p>Numer Kontaktowy</p>
+                    <p>323323323</p>
+                    <p>Mail</p>
+                    <p>Kamila@dsdas.pl</p>
+                </div> 
+            </span>
+            
+        </>
     )
 }
